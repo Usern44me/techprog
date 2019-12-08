a387a9444a4868e5e1cb267e5725e934ae6a9163
Usern4me <grisha19.03.2000@gmail.com>
2019-12-08 23:48:41 +0300

    WIP

--- techprog-spider/src/main/java/com/demo/techprog/spider/Launcher.java
+++ techprog-spider/src/main/java/com/demo/techprog/spider/Launcher.java
@@ -3,13 +3,15 @@ package com.demo.techprog.spider;
 import com.demo.techprog.spider.common.*;
 import com.demo.techprog.spider.config.Config;
 
+import java.io.IOException;
+
 public class Launcher {
 
     private static String writeUri;
     private static String readUri;
     private static Strategy strategy;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         init(args);
         strategy.execute();
     }
